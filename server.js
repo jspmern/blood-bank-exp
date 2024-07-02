ea8b8c76340447827e6742b5962e4e7e6da70384
jspmern <jspmern@gmail.com>
2024-07-02 14:28:29 +0530

    basic folder setup is done from be

---
+++ server.js
@@ -0,0 +1,19 @@
+//create Server
+let express = require('express')
+let app = express()
+let dotenv = require('dotenv')
+let cors = require('cors')
+let colors = require('colors')
+let morgan = require('morgan')
+//configration of dotenv
+dotenv.config()
+//app-level middlware
+app.use(express.json())
+//third party middlware
+app.use(cors())
+app.use(morgan('dev'))
+//PORT
+let PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is Running At  ${PORT}`.bgMagenta.white)
+})
