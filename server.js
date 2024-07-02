5a3fc0ca34713a101bd7f56f87e9700bb24e6063
jspmern <jspmern@gmail.com>
2024-07-02 14:35:07 +0530

    database connection done

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let dotenv = require('dotenv')
 let cors = require('cors')
 let colors = require('colors')
 let morgan = require('morgan')
+const databaseConnection = require('./config/db')
 //configration of dotenv
 dotenv.config()
 //app-level middlware
@@ -12,6 +13,8 @@ app.use(express.json())
 //third party middlware
 app.use(cors())
 app.use(morgan('dev'))
+//database connection
+databaseConnection()
 //PORT
 let PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
