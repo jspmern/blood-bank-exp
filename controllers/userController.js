a82e6e8964ac9551af1ad70c1e8c4068410bfbbd
jspmern <jspmern@gmail.com>
2024-07-04 13:41:03 +0530

    stash from main

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 let bcrypt = require('bcryptjs')
 const userModel = require("../models/user")
+const { raw } = require('express')
 //this is for the registration
 let registerController = async (req, res, next) => {
     try {
@@ -27,4 +28,34 @@ let registerController = async (req, res, next) => {
         })
     }
 }
-module.exports = { registerController }
+//this is for login and genrating token
+let loginController = async (req, res, next) => {
+    try {
+        if (!req.body.email || !req.body.password || !req.body.role) return res.status(401).send({ message: "All fields are required *" })
+        let findData = await userModel.findOne({ email: req.body.email })
+        if (!findData) return res.status(400).send({ message: "Either email or password is invalid", success: false })
+        //match-role
+        if (findData.role != req.body.role) throw new Error('Invalid Account')
+        //compare password
+        let comparePassword = await bcrypt.compare(req.body.password, findData.password)
+        if (!comparePassword) return res.status(400).send({ message: "Either email or password is invalid", success: false })
+        //create-token
+        let token = await jwt.sign({ userId: findData._id }, process.env.SECRET_KEY, { expiresIn: "20days" })
+        res.status(200).send({ message: "User is Login Successfully", success: true, token, user: findData })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: "Somthing wrong while login ", success: false, error })
+    }
+}
+//current-user details
+let getCurrentUserController = async (req, res, next) => {
+    try {
+        let userId = req.userId;
+        let user = await userModel.findOne({ _id: userId })
+        res.status(200).send({ message: "User Get SuccessFully", success: true, user })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: "Somthing wrong , while fetching current user", success: false, error })
+    }
+}
+module.exports = { registerController, loginController, getCurrentUserController }
