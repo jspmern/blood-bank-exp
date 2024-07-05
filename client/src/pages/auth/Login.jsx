e250862dec185ee6ea1b12b0e62779ad752a02b4
jspmern <jspmern@gmail.com>
2024-07-05 15:07:38 +0530

    reusable login , registration form is done

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Form from '../../component/shared/Form/Form'
 
 function Login() {
     return (
-        <div>Login</div>
+
+        <>
+            <div className="row g-0">
+                <div className="col-md-5">
+                    <img src='./assest/pexels-karolina-grabowska-4226923.jpg' alt="blood_bank_login" className='img-fluid login-image' />
+                </div>
+                <div className="col-md-6 form-container">
+
+                    <Form role="" submitText="submit" formTitle="Login" formType="login"/>
+
+                </div>
+            </div>
+        </>
     )
 }
 
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Form from '../../component/shared/Form/Form'
 
 function Register() {
     return (
-        <div>Register</div>
+
+        <>
+            <div className="row g-0">
+                <div className="col-md-5">
+                    <img src='./assest/pexels-karolina-grabowska-4226923.jpg' alt="blood_bank_login" className='img-fluid login-image' />
+                </div>
+                <div className="col-md-6 form-container">
+                    <Form submitText="Register" formTitle="Register" formType="register" />
+
+                </div>
+            </div>
+        </>
     )
 }
 
