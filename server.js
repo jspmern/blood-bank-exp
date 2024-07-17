ff8b66c2a65f06b09af39ef926cb9f84e2bbc9ba
jspmern <134581836+jspmern@users.noreply.github.com>
2024-07-18 01:43:23 +0530

    Analytics is done from frontend and backend

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const databaseConnection = require("./config/db");
 const route = require("./routes/auth");
 const inventoryRoute = require("./routes/inventroyRoute");
 const adminRoute = require("./routes/aminRoute");
+const analyticsRoute = require("./routes/analytics");
 //configration of dotenv
 dotenv.config();
 //app-level middlware
@@ -22,6 +23,7 @@ databaseConnection();
 app.use("/auth/v1", route);
 app.use("/inventory/v1", inventoryRoute);
 app.use("/admin/v1", adminRoute);
+app.use("/organization/v1", analyticsRoute);
 //PORT
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
